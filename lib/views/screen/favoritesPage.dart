dd989adad42414e595e1e101dd7b85099a6efabc
mahmod shenno <dev.mahmod.shenno@gmail.com>
2026-07-21 00:56:41 +0300

    feat: Add Favorites page with basic layout

---
+++ lib/views/screen/favoritesPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Favoritespage extends StatelessWidget {
+  const Favoritespage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Favorites Page'),
+    );
+  }
+}
