2866835a35c7b4406b8c24677d8398a3fb3e5236
mahmod shenno <dev.mahmod.shenno@gmail.com>
2026-07-21 01:18:27 +0300

    feat: Add SectionCard widget for reusable section display

---
+++ lib/views/screen/sectionCard.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SectionCard extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final VoidCallback? onTap;
+
+  const SectionCard({
+    super.key,
+    required this.title,
+    required this.icon,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(12),
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xFFEAF3E9),
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(color: const Color(0xFF235525), width: 1),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              size: 34,
+              color: const Color(0xFF235525),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              title,
+              textAlign: TextAlign.center,
+              style: GoogleFonts.cairo(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xFF235525),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
